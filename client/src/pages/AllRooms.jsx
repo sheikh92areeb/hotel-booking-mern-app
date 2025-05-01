15079148fd50be12f1946f2b032fa1b9d210744a
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-05-01 20:13:44 +0500

    working

---
+++ client/src/pages/AllRooms.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { assets, facilityIcons, roomsDummyData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import StarRating from '../components/StarRating'
+
+const AllRooms = () => {
+    const navigate = useNavigate()
+
+  return (
+    <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+      <div>
+        <div className='flex flex-col items-start text-left'>
+            <h1 className='font-playfair text-4xl md:text-[40px]'>Hotel Rooms</h1>
+            <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgotable memories.</p>
+        </div>
+
+        {roomsDummyData.map((room) => (
+            <div key={room._id} className='flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0'>
+                <img onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}} src={room.images[0]} alt="Hotel Img" title='View Room Details' className='max-h-65 md:w-1/2 rounded-xl shadow-lg object-cover cursor-pointer' />
+                <div className='md:w-1/2 flex flex-col gap-2'>
+                    <p className='text-gray-500'>{room.hotel.city}</p>
+                    <p onClick={()=> {navigate(`/rooms/${room._id}`); scrollTo(0,0)}} className='text-gray-800 text-3xl font-playfair cursor-pointer'>{room.hotel.name}</p>
+                    <div className='flex items-center'>
+                        <StarRating/>
+                        <p className='ml-2'>200+ Reviews</p>
+                    </div>
+                    <div className='flex items-center gap-1 text-gray-500 text-sm mt-2'>
+                        <img src={assets.locationIcon} alt="Location Icon" />
+                        <span>{room.hotel.address}</span>
+                    </div>
+                    {/* Room Amenities */}
+                    <div className='flex flex-warp items-center gap-4 mt-3 mb-6'>
+                        {room.amenities.map((item, index) => (
+                            <div key={index} className='flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70'> 
+                                <img src={facilityIcons[item]} alt={item} className='w-5 h-5' />
+                                <p className='text-xs'>{item}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+            </div>
+        ))}
+
+      </div>
+      {/* Filters */}
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default AllRooms
