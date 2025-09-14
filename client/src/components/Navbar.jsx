cb2d05d7d52f3d931d9aee4a822e0e39710bf114
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-09-14 19:48:31 +0500

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from '../assets/assets'
 import { useClerk, useUser, UserButton } from "@clerk/clerk-react";
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Hero from '../components/Hero'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
