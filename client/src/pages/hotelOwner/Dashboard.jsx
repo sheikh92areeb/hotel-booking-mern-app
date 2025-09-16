4e82e4eded8c68857b1fe4d2cfcc35d2a06d1109
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-09-16 19:56:25 +0500

    updated

---
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import Title from '../../components/Title'
+import { assets, dashboardDummyData } from '../../assets/assets'
+
+const Dashboard = () => {
+    const [dashboardData, setDashboardData] = useState(dashboardDummyData)
+
+  return (
+    <div>
+      <Title align='left' font='outfit' title='Dashboard' subtitle='Monitor your room listings, track bookings and analyze revenue-all in one place. stay updated with real time insights to ensure smooth operations.' />
+
+      <div className="flex gap-4 my-8">
+        {/* total bookings */}
+        <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+            <img src={assets.totalBookingIcon} alt="total booking icon" className='max-sm:hidden h-10' />
+            <div className="flex flex-col sm:ml-4 font-medium">
+                <p className='text-blue-500 text-lg'>Total Bookings</p>
+                <p className='text-neutral-400 text-base'>{dashboardData.totalBookings}</p>
+            </div>
+        </div>
+
+        {/* total revenue */}
+        <div className="bg-primary/3 border border-primary/10 rounded flex p-4 pr-8">
+            <img src={assets.totalRevenueIcon} alt="total revenue icon" className='max-sm:hidden h-10' />
+            <div className="flex flex-col sm:ml-4 font-medium">
+                <p className='text-blue-500 text-lg'>Total Revenue</p>
+                <p className='text-neutral-400 text-base'>$ {dashboardData.totalRevenue}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -1,9 +1,17 @@
+import { Outlet } from "react-router-dom"
 import Navbar from "../../components/hotelOwner/Navbar"
+import Sidebar from "../../components/hotelOwner/Sidebar"
 
 const Layout = () => {
   return (
     <div className="flex flex-col h-screen">
         <Navbar />
+        <div className="flex h-full">
+          <Sidebar />
+          <div className="flex-1 p-4 pt-10 md:px-10 h-full">
+            <Outlet />
+          </div>
+        </div>
     </div>
   )
 }
